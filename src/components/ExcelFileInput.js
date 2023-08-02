a12ca25f1b913c5d3405bd2dd44da5d44aafa1ab
AvinashEatoes <avinash@eatoes.com>
2023-08-02 11:03:47 +0530

    done

--- src/components/ExcelFileInput.js
+++ src/components/ExcelFileInput.js
@@ -1,12 +1,16 @@
-import React, { useState } from 'react';
+import React from 'react';
 import * as XLSX from 'xlsx';
 
 const ExcelFileInput = ({ onDataExtracted }) => {
   const handleChange = (event) => {
     const file = event.target.files[0];
-    const reader = new FileReader();
-    
 
+    if (!file) {
+      console.error('Error: No file selected.');
+      return;
+    }
+
+    const reader = new FileReader();
     reader.onload = (e) => {
       try {
         const data = new Uint8Array(e.target.result);
@@ -15,8 +19,25 @@ const ExcelFileInput = ({ onDataExtracted }) => {
         const worksheet = workbook.Sheets[firstSheetName];
 
         if (worksheet && worksheet['!ref']) {
-          const json = XLSX.utils.sheet_to_json(worksheet, { header: 1 });
-          onDataExtracted(json);
+          // Convert the worksheet to an array of arrays
+          const rows = XLSX.utils.sheet_to_json(worksheet, { header: 1 });
+
+          // Filter out empty rows (rows with all cells empty are considered empty)
+          const nonEmptyRows = rows.filter((row) => row.some((cell) => cell !== ''));
+
+          // Convert the filtered rows (excluding header) to JSON
+          const jsonData = [];
+          for (let i = 1; i < nonEmptyRows.length; i++) {
+            const rowObj = {};
+            for (let j = 0; j < nonEmptyRows[i].length; j++) {
+              const cellValue = nonEmptyRows[i][j];
+              const headerValue = nonEmptyRows[0][j];
+              rowObj[headerValue] = cellValue;
+            }
+            jsonData.push(rowObj);
+          }
+
+          onDataExtracted(jsonData);
         } else {
           console.error('Error: The Excel file is empty or invalid.');
         }
@@ -32,14 +53,10 @@ const ExcelFileInput = ({ onDataExtracted }) => {
 
     reader.readAsArrayBuffer(file);
   };
-  const [input,setInput]=useState("");
-  const clicked=()=>{
-    setInput("");
-  }
 
   return (
     <div>
-      <input type="file" accept=".xlsx, .xls" value={input} onChange={handleChange} onClick={clicked} className='my-7'/>
+      <input type="file" accept=".xlsx, .xls" onChange={handleChange} />
     </div>
   );
 };
